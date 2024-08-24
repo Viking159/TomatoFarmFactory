6e68a1213e5182ec99ed228c9e8c881011903631
Viking159 <IiIgor8@yandex.ru>
2024-08-25 01:57:50 +0700

    Consume animation control

--- Assets/GameResources/Data/GlobalData.cs
+++ Assets/GameResources/Data/GlobalData.cs
@@ -13,6 +13,6 @@ namespace Features.Data
         /// <summary>
         /// Covert fabric consume speed to hand animation speed ratio
         /// </summary>
-        public const int FABRIC_HAND_ANIMATION_SPEED_CONVERT_RATIO = 4;
+        public const int FABRIC_HAND_ANIMATION_SPEED_CONVERT_RATIO = 2;
     }
 }
